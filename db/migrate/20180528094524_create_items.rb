eff47dbb6a0ba8ae41ec53adac76fd8a35d7e416
Itspacome <pacome.perrichot@gmail.com>
2018-05-28 12:20:50 +0200

    table qui fonctionne

--- db/migrate/20180528094524_create_items.rb
+++ db/migrate/20180528094524_create_items.rb
@@ -1,8 +1,16 @@
 class CreateItems < ActiveRecord::Migration[5.2]
-  def change
-    create_table :items do |t|
 
-      t.timestamps
-    end
-  end
+	def change
+    	create_table :items do |prod|
+    		prod.string :title
+    		prod.text :description
+    		prod.decimal :price
+    		prod.string :image_url
+
+    		prod.timestamps
+
+    	end
+		
+	end
+
 end
