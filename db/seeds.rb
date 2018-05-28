59787b6ec979b2117d3e5004e34d6d465fb284ae
Jordane <faou.jordane@gmail.com>
2018-05-28 12:56:53 +0200

    test seed1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# table Item
+# title: string
+# description: text
+# price: decimal 
+# image_url: string  <img src="<%= @item.image_url %>">
+require 'faker'
+
+20.times do
+    Items.create([{
+        title: Faker::Cat.name,
+        description: Faker::Cat.breed,
+        price: Faker::Commerce.price,
+        image_url: seed_image()
+    }])
+    end
+
+        def seed_image()
+            File.open(File.join(Rails.root, "/app/assets/images/cats/#{file_name}.jpg"))
+        end
+
+#    <img src="<%= @item.image_url %>"
