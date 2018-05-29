2ab3fd1a90f34d004564cdb4189e1e65b2a76ae3
Selma <asakab@live.fr>
2018-05-29 20:10:15 +0200

    rien a changé

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -7,12 +7,14 @@ class IndexController < ApplicationController
   end
   
   def cart
+
   end
 
   def add_to_cart
     if current_user.cart == nil
       current_user.create_cart
     end
+
     item = Item.find(params[:id])
     current_user.cart.items << item
     p current_user.cart.items
