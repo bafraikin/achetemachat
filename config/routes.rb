07e2e4809a4efb0fedd7f5f7e0363d603dbb15b9
Selma <asakab@live.fr>
2018-05-30 17:04:01 +0200

    ajout de stripe

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   resources :carts, only: [:new]
   get 'show', to: 'carts#show', as: 'pay'
+  resources :charges
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
