86c14a07d3e3b012b98f66de7b736d1537297ec9
Itspacome <pacome.perrichot@gmail.com>
2018-05-31 12:16:35 +0200

    mailer mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,20 @@
 class UserMailer < ApplicationMailer
+	def order_mailer
+
+  		@order = params [:order]
+  		@user = order.user
+	
+    	mail(to: @user.email, subject: "Machat order!", reply_to: 'bob.chamo@laposte.net') do |format|
+    		format.html {render 'user_mailer/user_order_mailer'}
+		end
+	end
+
+	def admin_mailer
+	
+	    @order = params[:order]
+	
+	    mail(to: 'bob.chamo@laposte.net', subject: "New Order", reply_to: 'bob.chamo@laposte.net') do |format|
+	      format.html { render 'user_mailer/admin_order_mailer'}
+	    end
+	end
 end
